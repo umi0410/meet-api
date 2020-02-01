ca8fbd8224755a5c9f91594f534614e4fefe963b
umi <bo314@naver.com>
2020-02-02 03:17:19 +0900

    CodeBuild

--- codepipeline-scripts/initialize.sh
+++ codepipeline-scripts/initialize.sh
@@ -1,4 +1,3 @@
 cd /home/ubuntu/meet-api
 docker-compose down
 rm -Rf "/home/ubuntu/meet-api"
-
--- codepipeline-scripts/start.sh
+++ codepipeline-scripts/start.sh
@@ -1,2 +1,3 @@
 cd /home/ubuntu/meet-api
+$(aws ecr get-login --region ap-northeast-2 --no-include-email)
 docker-compose up -d

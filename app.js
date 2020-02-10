7a2f8ecc576b37e24e8603e1a01505257aa61451
umi <bo314@naver.com>
2020-02-10 16:05:48 +0900

    FCM 요청 보낼 때 Content-type 정의해줘야됨

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const questionsRouter = require("./routes/questions");
 const tagsRouter = require("./routes/tags");
 const universitiesRouter = require("./routes/universities");
 // const webPushRouter = require("./routes/webPush");
-// const pushesRouter = require("./routes/pushes");
+const pushesRouter = require("./routes/pushes");
 const PushSubscription = require("./models/PushSubscription");
 const Message = require("./models/Message");
 const User = require("./models/User");
@@ -136,6 +136,7 @@ app.io.on("connection", function(socket) {
 						}
 					}),
 					headers: {
+						"Content-Type": "application/json",
 						Authorization:
 							"key=" + process.env.FIREBASE_AUTHORIZATION
 					}

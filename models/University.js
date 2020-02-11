97209807cb6a5119a157bd4805e328672ff7ec5c
umi <bo314@naver.com>
2020-02-11 18:28:15 +0900

    university campus

--- models/University.js
+++ models/University.js
@@ -1,12 +1,19 @@
 const Kyunghee = {
-	universityName: "Kyunghee",
+	universityName: "경희",
 	domain: "khu.ac.kr",
-	majors: ["사학과", "철학과", "영어영문학과", "응용영어통번역학과", "법학부"]
+	majors: [
+		"사학과",
+		"철학과",
+		"영어영문학과",
+		"응용영어통번역학과",
+		"법학부"
+	],
+	campuses: ["국제", "서울"]
 };
 const Ajou = {
-	universityName: "Ajou",
+	universityName: "아주",
 	domain: "ajou.ac.kr",
-	marjors: [
+	majors: [
 		"글로벌비즈니스전공",
 		"기계공학과",
 		"산업경영공학과",
@@ -14,7 +21,30 @@ const Ajou = {
 		"화학공학과"
 	]
 };
+const Dankuk = {
+	universityName: "단국",
+	domain: "dku.ac.kr",
+	majors: [
+		"글로벌비즈니스전공",
+		"응용영어통번역학과",
+		"법학부",
+		"원자력공학과",
+		"화학공학과"
+	]
+};
+const Seong = {
+	universityName: "성균관",
+	domain: "skku.ac.kr",
+	majors: [
+		"글로벌비즈니스전공",
+		"응용영어통번역학과",
+		"법학부",
+		"원자력공학과",
+		"화학공학과"
+	],
+	campuses: ["서울", "수원"]
+};
 
 module.exports = {
-	universities: [Kyunghee, Ajou]
+	universities: [Kyunghee, Ajou, Dankuk, Seong]
 };

d0951d9966cf50a7edebd7ea07c1b2c8b0d7d014
umi <bo314@naver.com>
2020-02-13 05:35:20 +0900

    docker compose env

--- codepipeline-scripts/start.sh
+++ codepipeline-scripts/start.sh
@@ -1,4 +1,4 @@
 cd /home/ubuntu/meet-api
+cp /home/ubuntu/meet-api-env/ /home/ubuntu/meet-api/
 $(aws ecr get-login --region ap-northeast-2 --no-include-email)
-docker-compose build --no-cache
 docker-compose up -d
